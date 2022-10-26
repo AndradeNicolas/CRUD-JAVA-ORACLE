8a6d4f033caed55b906c94adf494d6b623c5a48f
AndradeNicolas <nicolas.andrade@segurosbolivar.com>
2022-10-26 08:26:01 -0500

    se eliminan comentarios

--- src/CRUD/App.java
+++ src/CRUD/App.java
@@ -1,44 +1,12 @@
-import  java.sql.*;
+package CRUD;
 
 import controller.CtrlPersona;
 import model.PersonaDAO;
 import model.PersonaDTO;
 import view.VistaPersona;  
 public class App {
-    // private static final String DB_DRIVER = 
-	// 	           "oracle.jdbc.driver.OracleDriver";
-	// private static final String DB_URL = 
-	// 	        "jdbc:oracle:thin:@localhost:1521:XE";
-	// private static final String DB_USERNAME = "system";
-	// private static final String DB_PASSWORD = "oracle";
     public static void main(String[] args) throws Exception {
-        // Connection conn = null;
-		// try{
-		// 	//Register the JDBC driver
-		// 	Class.forName(DB_DRIVER);
- 
-		// 	//Open the connection
-		// 	conn = DriverManager.
-		// 	getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
-			
-			
-
-		// 	if(conn != null){
-		// 	   System.out.println("Successfully connected.");
-		// 	}else{
-		// 	   System.out.println("Failed to connect.");
-		// 	}
-		// 	//create the statement object
-        //     Statement stmt = conn.createStatement();
-		// 	stmt.executeQuery("ALTER SESSION SET CURRENT_SCHEMA=test");
-		// 	// stmt.executeQuery("INSERT INTO PERSONA (FIRST_NAME, LAST_NAME) VALUES ('primero', 'segundo')");
-		// 	ResultSet rs=stmt.executeQuery("select * from persona");  
-		// 	while(rs.next())
-		// 		System.out.println(rs.getInt(1) + " " + rs.getString(2) + " " + rs.getString(3));
-
-		// }catch(Exception e){
-		// 	e.printStackTrace();
-		// }
+       
         
 		PersonaDAO personaDAO = new PersonaDAO();
 		PersonaDTO personaDTO = new PersonaDTO();
